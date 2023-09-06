679ddbb87336181d49076207272416d6c36d89be
GHOST-Aram <owinoojala@gmail.com>
2023-09-06 06:01:44 +0300

    feat: Creat Nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import SignUp from './pages/SignUp';
 import Login from './pages/Login';
 import { Routes, Route } from 'react-router-dom';
 import Posts from './pages/Posts';
+import Navbar from './core-components/Navbar';
 
 const  App = () => {
 	return (
 		<div className="App">
+			<Navbar />
 			<Routes>
 				<Route path = '/login' element={<Login/>} />
 				<Route path= '/signup' element = {<SignUp />} />
---
+++ src/core-components/Navbar.jsx
@@ -0,0 +1,89 @@
+import React from 'react'
+import {
+    AppBar,
+    Toolbar,
+    Button,
+    Menu,
+    MenuItem,
+    Typography,
+    Stack,
+    Avatar,
+    IconButton
+} from '@mui/material'
+import {
+    CatchingPokemon,
+    KeyboardArrowDown,
+    Person,
+    HomeRounded,
+    PeopleAltRounded,
+    NotificationAddRounded
+} from '@mui/icons-material'
+import {useNavigate, NavLink, Link} from 'react-router-dom' 
+
+
+
+const Navbar = () => {
+    const navigate = useNavigate()
+
+    const navigateToPosts = () =>{
+        navigate('/posts')
+    }
+
+    return (
+        <AppBar>
+            <Toolbar 
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-between'
+                }}
+            >
+                <Button
+                    onClick={navigateToPosts}
+                    startIcon = {<CatchingPokemon />}
+                    color='inherit'
+                >
+                    OdinBook
+                </Button>
+                <Stack direction={'row'} spacing={4}>
+                    <NavLink 
+                        style={{
+                            color: 'white'
+                        }}
+                        to={'/posts'}
+                    >
+                        <IconButton size='large' color='inherit'><HomeRounded /></IconButton>
+                    </NavLink>
+                    <NavLink
+                        to={'/people'}
+                    >
+                        <IconButton size='large'><PeopleAltRounded /></IconButton>
+                    </NavLink>
+                    <NavLink
+                        to={'/notifications'}
+                    >
+                        <IconButton size='large'><NotificationAddRounded /></IconButton>
+                    </NavLink>
+                </Stack>
+                <Stack 
+                    direction={'row'}
+                    justifyContent={'flex-end'}
+                    spacing={2}
+                >
+                    <Avatar>
+                        <Link to={'/profile'}>
+                            <Person />
+                        </Link>
+                    </Avatar>
+                    <Button 
+                        color='inherit'
+                        endIcon = {<KeyboardArrowDown />}
+                    >Erick Jackson</Button>
+                    
+                </Stack>
+            </Toolbar>
+        </AppBar> 
+    )
+}
+
+export default Navbar
